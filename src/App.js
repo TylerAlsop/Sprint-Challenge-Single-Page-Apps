4485af809788f78d5aeb653b79ae47a847b58b64
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-24 19:22:30 -0700

    Imported Route into App.js but didn't do anything with it yet.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState }  from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
+
 
 
 export default function App() {
@@ -9,3 +11,40 @@ export default function App() {
     </main>
   );
 }
+
+
+
+
+
+
+
+
+// For Reference
+
+// import React, { useState } from 'react';
+// import { Route } from "react-router-dom";
+// import MovieList from "./Movies/MovieList";
+// import Movie from "./Movies/Movie";
+
+
+// import SavedList from './Movies/SavedList';
+
+// const App = () => {
+//   const [savedList, setSavedList] = useState( [] );
+
+//   const addToSavedList = movie => {
+//     setSavedList( [...savedList, movie] );
+//   };
+
+//   return (
+//     <div>
+//       <SavedList list={savedList} />
+      
+//       <Route exact path="/" component={MovieList} />
+//       <Route path="/movies/:id" component={Movie} />
+
+//     </div>
+//   );
+// };
+
+// export default App;
