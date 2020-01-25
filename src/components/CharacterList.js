e0d5d511bc9ce3b20602ba635d295e3526b4bccd
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-24 18:57:39 -0700

    Added useState in CharacterList.js to track data from useEffect

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [charcter, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
