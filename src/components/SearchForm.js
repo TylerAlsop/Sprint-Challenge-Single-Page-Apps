ebc73d5c7edcffd978ce5ba0e1300ab235931a6c
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-24 19:26:34 -0700

    Added two useStates to SearchForm.js for search, and filteredCharacters.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,66 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
+  const [search, setSearch] = useState("");
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
+
  
   return (
     <section className="search-form">
      // Add a search form here
     </section>
+  );
+}
+
+
+
+
+
+
+
+For Reference 
+
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const colors = [
+  "red",
+  "blue",
+  "dodgerblue",
+  "darkred",
+  "purple",
+  "green",
+  "grey"
+];
+
+export default function App() {
+  const [search, setSearch] = useState("");
+  const [filteredColors, setFilteredColors] = useState([]);
+
+  useEffect(() => {
+    const filtered = colors.filter(colorName => colorName.includes(search));
+    setFilteredColors(filtered);
+  }, [search]);
+
+  const handleChange = e => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>The Magic of Filtering</h1>
+      <input
+        type="text"
+        placeholder="Start typing here to see the magic happen!"
+        onChange={handleChange}
+        value={search}
+      />
+      {filteredColors.map(colorName => (
+        <div key={colorName} style={{ color: colorName }}>
+          {colorName}
+        </div>
+      ))}
+    </div>
   );
 }
