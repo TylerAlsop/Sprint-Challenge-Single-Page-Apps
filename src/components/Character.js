2a77295d19d1dc4baf254572fe385928f76d43cf
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-24 18:15:00 -0700

    Created markup for CharacterCard.js and imported CharacterCard into Character.js to render there

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,32 +1,43 @@
 import React from 'react';
+import CharacterCard from "./CharacterCard"
 
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
-export default function CharacterList() {
+export default function Character(props) {
   // TODO: Add useState to track data from useEffect
-  const [characterInfo, setCharacterInfo] = useState([]);
+  const [character, setCharacter] = useState();
+  console.log(props);
+
   
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    const id = 
+    const id = props.match.params.id
+    console.log("id: ", id);
 
 
     axios
-      .get("https://rickandmortyapi.com/api/character/")
+      .get(`https://rickandmortyapi.com/api/character/${id}`)
       .then(
         res => {
         console.log("Success:", res);}
       )
       .catch(err => console.log("Error", err.response))
 
-  }, []);
+  }, [props.match.params.id]);
+
+  if (!character) {
+    return <div>Loading character information...</div>;
+  }
+
+  const { id, name, status, species, type } = character;
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      <CharacterCard />
       
     </section>
   );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <div className="character-card">
+        <h2 className="character-name">{name}</h2>
+        <div className="character-status">
+          Status: <strong>{status}</strong>
+        </div>
+        <div className="character-species">
+          Species: <strong>{species}</strong>
+        </div>
+        <div className="character-type">
+          Type: <strong>{type}</strong>
+        </div>
+
+        {/* {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+        ))} */}
+    </div>
+  )
 }
