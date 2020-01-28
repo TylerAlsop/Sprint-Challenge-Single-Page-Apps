f2358f851816e663fe55ff0354aacf8ee7ac6307
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-27 19:53:30 -0700

    Removed the props just added to SearchForm.js and re-commented out the SearchForm properties in CharacterList.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-export default function SearchForm(props) {
+export default function SearchForm() {
   const [search, setSearch] = useState("");
   const [filteredCharacters, setFilteredCharacters] = useState([]);
 
   useEffect(() => {
-    const filtered = props.filter(characterName => characterName.includes(search));
+    const filtered = characterInfo.filter(characterName => characterName.includes(search));
     setFilteredCharacters(filtered);
   }, [search]);
 
