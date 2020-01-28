237f96ee61c36c3a79c00ea66c83114ebe109e3e
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-27 19:58:50 -0700

    Deleted the SearchForm code from CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -30,47 +30,11 @@ export default function CharacterList() {
     getCharacterInfo();
 
   }, []);
-
-  function SearchForm() {
-    const [search, setSearch] = useState("");
-    const [filteredCharacters, setFilteredCharacters] = useState([]);
-  
-    useEffect(() => {
-      const filtered = characterInfo.filter(characterName => characterName.includes(search));
-      setFilteredCharacters(filtered);
-    }, [search]);
-  
-    const handleChange = e => {
-      e.preventDefault();
-      setSearch(e.target.value);
-    };
-  
-   
-    return (
-      <div className="search-form">
-        <h1>Search Characters Here:</h1>
-        <input
-          type="text"
-          placeholder="Search Character Name Here"
-          onChange={handleChange}
-          value={search}
-        />
-        {filteredCharacters.map(characaterName => (
-          <div key={characaterName}>
-            {characterName}
-          </div>
-        ))}
-  
-      </div>
-    );
-  }
-
   
 
   return (
     <div>
       <section className="search-bar">
-        {SearchForm}
         {/* <SearchForm /> */}
       </section>
       <section className="character-list">
